58947e78b2a8ebeb58c8756de012ee846b2f0af0
NoahTheDuke <nbtheduke@gmail.com>
2015-12-18 17:24:02 -0500

    Working on grid test. Trying to find edges.

--- grid_test.py
+++ grid_test.py
@@ -1,5 +1,4 @@
 import PyBearLibTerminal as blt
-import random
 
 WINDOW_WIDTH = 80
 WINDOW_HEIGHT = 45
@@ -13,31 +12,73 @@ class Tile:
 
 
 class Quadrant:
-    _size = 325
-    _street_width = 5
+    size = 325
+    small_size = 25
+    street_width = 5
 
     def __init__(self):
         self.layout = [[Tile(x, y)
-                       for y in range(self._size)]
-                       for x in range(self._size)]
+                       for y in range(self.small_size)]
+                       for x in range(self.small_size)]
 
-    def place_streets(self):
-        num_streets = 4
+        self.make_roads()
+        self.make_blocks()
+
+    def make_roads(self):
+        self.all_nodes = [[x * 2, y * 2]
+                          for y in range(6)
+                          for x in range(6)]
+
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        result = []
+        for node in self.all_nodes:
+            for dr in dirs:
+                neighbor = [node[0] + dr[0], node[1] + dr[1]]
+                if 0 <= neighbor[0] < 11 and 0 <= neighbor[1] < 11:
+                    if neighbor not in result:
+                        result.append(neighbor)
+        print(result)
+        for (x, y) in result:
+            d, m = divmod(x, 5)
+            if m > 0 and m != 4:
+                print(m)
+        self.roads = result
+
+    def make_blocks(self):
+        self.blocks = [[x, y]
+                       for x in range(0, self.small_size, 5)
+                       for y in range(0, self.small_size, 5)]
+        self.make_lots()
+
+    def make_lots(self):
+        self.make_houses()
+
+    def make_houses(self):
+        pass
 
 
 def main():
     global WINDOW_WIDTH, WINDOW_HEIGHT, CELLSIZE
     blt.open_()
-    blt.set_("window: size={}x{}, cellsize={}, title='Roguelike';"
+    blt.set_("window: size={}x{}, cellsize={}, title='Grid Test';"
              "font: default".format(
                  str(WINDOW_WIDTH), str(WINDOW_HEIGHT), CELLSIZE))
     blt.clear()
     blt.refresh()
     blt.color("white")
-    seed = random.randint(0, 10)
-    print(seed)
-    # g = Quadrant()
-    # print([(b.x, b.y) for a in g.layout for b in a])
+    g = Quadrant()
+    blt.clear()
+    for x in range(30):
+        for y in range(30):
+            if [x, y] in g.all_nodes:
+                blt.print_(x * 2, y * 2, ' ')
+            elif [x, y] in g.roads:
+                xd, xm = divmod(x, 5)
+                yd, ym = divmod(y, 5)
+                print(xd, xm)
+                print(yd, ym)
+                blt.print_(x * 2, y * 2, str(g.roads.index([x, y])))
+    blt.refresh()
 
     proceed = True
     while proceed:
@@ -46,7 +87,6 @@ def main():
             key = blt.read()
             if key == blt.TK_CLOSE or key == blt.TK_Q:
                 proceed = False
-    # if proceed is False, end the program
     blt.close()
 
 if __name__ == '__main__':
